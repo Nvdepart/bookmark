eeb61cbb927467972298432c1b5ac05693db79df
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-04-16 08:03:12 +0200

    navbar responsive

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,6 @@
-const hamMenu = document.querySelector(".hamburger-menu");
-const offScreenMenu = document.querySelector(".off-screen-menu");
+const toggleBtn = document.querySelector("#toggle");
 const navList = document.querySelector(".nav_list");
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-  navList.classList.toggle("nav_list-show");
-});
-/* hamburger.forEach((element) => {
-  element.addEventListener("click", () => {
-    //element.classList.toggle();
 
-    menu.classList.toggle("nav_list-show");
-  });
-}); 
-hamburger.addEventListener("click", () => {
-  hamburger.src = "./images/icon-close.svg";
-  menu.classList.toggle("nav_list-show");
-});*/
+toggleBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
